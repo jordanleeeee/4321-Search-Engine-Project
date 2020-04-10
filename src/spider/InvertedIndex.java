d60a53917202100e15c3a7f51021d2b79e4a86e6
Lee Tsz Leung <jordanlee80@gmail.com>
2020-04-10 17:20:08 +0800

    basic function

---
+++ src/spider/InvertedIndex.java
@@ -0,0 +1,4 @@
+package spider;
+
+public class InvertedIndex {
+}
--- src/spider/PageProperty.java
+++ src/spider/PageProperty.java
@@ -5,94 +5,86 @@ import org.rocksdb.*;
 import java.util.*;
 
 public class PageProperty {
-    private Set<String> fetchedPage = new HashSet<>();
+    private final String PATH = "/Java/4321project/pagePropDB";
     private RocksDB db;
-    private List<ColumnFamilyDescriptor> colFamily = new Vector<>();
     private List<ColumnFamilyHandle> handles = new Vector<>();
-    private int numOfPage;
 
-    PageProperty(String url, int numOfPage) throws RocksDBException {
+    PageProperty() throws RocksDBException {
         try {
-            db = RocksDB.open("/Java/4321project/db");
+            db = RocksDB.open(PATH);
             db.createColumnFamily(new ColumnFamilyDescriptor("title".getBytes()));
             db.createColumnFamily(new ColumnFamilyDescriptor("url".getBytes()));
             db.createColumnFamily(new ColumnFamilyDescriptor("lastDateOfModification".getBytes()));
             db.createColumnFamily(new ColumnFamilyDescriptor("size".getBytes()));
             db.close();
-        }catch (RocksDBException ignored){ }
+        } catch (RocksDBException ignored){ }
 
-
+        List<ColumnFamilyDescriptor> colFamily = new Vector<>();
         colFamily.add(new ColumnFamilyDescriptor(RocksDB.DEFAULT_COLUMN_FAMILY, new ColumnFamilyOptions()));
         colFamily.add(new ColumnFamilyDescriptor("title".getBytes(), new ColumnFamilyOptions()));
         colFamily.add(new ColumnFamilyDescriptor("url".getBytes(), new ColumnFamilyOptions()));
         colFamily.add(new ColumnFamilyDescriptor("lastDateOfModification".getBytes()));
         colFamily.add(new ColumnFamilyDescriptor("size".getBytes()));
-        db = RocksDB.open(new DBOptions(), "/Java/4321project/db", colFamily, handles);
-        this.numOfPage = numOfPage;
-        DFS(url);
+        db = RocksDB.open(new DBOptions(), PATH, colFamily, handles);
     }
 
-    private Set<String> getFetchedPage() {
-        return fetchedPage;
+    public RocksDB getDb() {
+        return db;
     }
 
-    private void store(int id, String url) throws RocksDBException {
-        db.put(Integer.toString(id).getBytes(), "url".getBytes());
+    String getLastModificationTime(int id) {
+        try {
+            return new String(db.get(handles.get(2), String.valueOf(id).getBytes()));
+        } catch (RocksDBException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void delEntry(byte[] key) throws RocksDBException {
+        for(ColumnFamilyHandle h: handles){
+            db.delete(h, key);
+        }
+    }
+
+    void clearDataBase() throws RocksDBException {
+        RocksIterator iter = db.newIterator();
+        for(iter.seekToFirst(); iter.isValid(); iter.next()) {
+            delEntry(iter.key());
+        }
+    }
+
+    void store(int id, String url){
+        //db.put(Integer.toString(id).getBytes(), "url".getBytes());
         WebInfoSeeker seeker = new WebInfoSeeker(url);
-        db.put(handles.get(0), Integer.toString(id).getBytes(), seeker.fetchTitle().getBytes());
-        db.put(handles.get(1), Integer.toString(id).getBytes(), url.getBytes());
-        db.put(handles.get(2), Integer.toString(id).getBytes(), seeker.fetchLastModificationTime().getBytes());
-        db.put(handles.get(3), Integer.toString(id).getBytes(), seeker.fetchSize().getBytes());
+        try {
+            db.put(handles.get(0), Integer.toString(id).getBytes(), seeker.getTitle().getBytes());
+            db.put(handles.get(1), Integer.toString(id).getBytes(), url.getBytes());
+            db.put(handles.get(2), Integer.toString(id).getBytes(), seeker.getLastModificationTime().getBytes());
+            db.put(handles.get(3), Integer.toString(id).getBytes(), seeker.getPageSize().getBytes());
+        } catch (RocksDBException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void delEntry(int id) throws RocksDBException {
-        db.remove(Integer.toString(id).getBytes());
-    }
-
-    private void printAll() throws RocksDBException {
-        for (int i = 1; i < numOfPage+1; i++) {
-//            System.out.print(new String(db.get(Integer.toString(i).getBytes())));
-            System.out.print(i+": ");
-            System.out.print(new String(db.get(handles.get(0), Integer.toString(i).getBytes())));
+    void printAll() throws RocksDBException {
+        RocksIterator iterator = db.newIterator();
+        for(iterator.seekToFirst(); iterator.isValid(); iterator.next()) {
+            System.out.print(new String(iterator.key())+": ");
+            System.out.print(new String(db.get(handles.get(0), iterator.key())));
             System.out.print("\t");
-            System.out.print(new String(db.get(handles.get(1), Integer.toString(i).getBytes())));
+            System.out.print(new String(db.get(handles.get(1), iterator.key())));
             System.out.print("\t");
-            System.out.print(new String(db.get(handles.get(2), Integer.toString(i).getBytes())));
+            System.out.print(new String(db.get(handles.get(2), iterator.key())));
             System.out.print("\t");
-            System.out.print(new String(db.get(handles.get(3), Integer.toString(i).getBytes())));
+            System.out.print(new String(db.get(handles.get(3), iterator.key())));
             System.out.println();
         }
     }
 
-    private void DFS(String url) throws RocksDBException {
-        Queue<String> queue = new LinkedList<>();
-        queue.add(url);
-        int i = 0;
-        while (!queue.isEmpty()) {
-            String site = queue.remove();
-            if(fetchedPage.size() >= numOfPage){
-                break;
-            }
-            if (fetchedPage.contains(site)) {
-                continue;
-            }
-            System.out.println("site "+ i++);
-            fetchedPage.add(site);
-            store(i, site);
-            System.out.println("handling " + site);
-            List<String> links = WebInfoSeeker.getChildLinks(site);
-            for (String link : links) {
-                if (!fetchedPage.contains(link)) {
-                    queue.add(link);
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) throws RocksDBException {
-        PageProperty fetcher = new PageProperty("https://www.cse.ust.hk",30);
-        System.out.println("the database contain...");
+        PageProperty fetcher = new PageProperty();
+        fetcher.clearDataBase();
         fetcher.printAll();
-        //Set<String> fetchedPage = fetcher.getFetchedPage();
     }
 }
